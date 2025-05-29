17d0af9a27255ea6b510cf42f538465ab5f930bb
Benkralex <75564514+Benkralex@users.noreply.github.com>
2025-05-30 00:45:07 +0200

    Update exam.js

--- www/scripts/exam.js
+++ www/scripts/exam.js
@@ -42,7 +42,21 @@ if (id) {
                     modal.className = 'modal-dialog modal-dialog-centered fade';
                     modal.id = pic;
                     modal.innerHTML = `
-                        <img src="/data/${pic}" class="modal-content" style="max-width: 100%; max-height: 100%;">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="/data/${pic}" class="modal-content" style="max-width: 100%; max-height: 100%;">
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                            </div>
+                        </div>
                     `;
                     document.body.appendChild(modal);
                 }
